fd0bfee3ab9b938c14b550db847faf2185663d13
mnikn <329121948@qq.com>
2022-09-18 23:19:27 +0800

    feat: update

--- src/renderer/utils/file.ts
+++ src/renderer/utils/file.ts
@@ -2,13 +2,15 @@ export function getBaseUrl(filePath: string) {
   const p = filePath.replace(/\\/g, '/');
   const p1 = p.split('.st')[0];
   const fileName = p1.split('/')[p1.split('/').length - 1];
-  const baseUrl = p1
-    .split('/')
-    .filter((item) => item !== fileName)
-    .join('\\');
+  // const baseUrl = p1
+  //   .split('/')
+  //   .filter((item) => item !== fileName)
+  //   .join('\\');
+  let baseUrl = p1.split('/').join('\\');
+  baseUrl = baseUrl.substring(0, baseUrl.lastIndexOf('\\'));
   return baseUrl;
 }
 
 export function concatPath(baseUrl: string, path: string) {
   return baseUrl + '\\' + path;
-  }
+}
